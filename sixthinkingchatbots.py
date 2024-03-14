4bcfe4b0c61dc50701cd166b636da1c2acc26f58
rachmann-alexander <98393837+rachmann-alexander@users.noreply.github.com>
2024-03-14 15:03:08 +0000

    Further Refactoring to include more modes

--- sixthinkingchatbots.py
+++ sixthinkingchatbots.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 import time
 from openai import OpenAI
+import logging
 
 from config import API_KEY
 
@@ -64,23 +65,95 @@ class SixThinkingChatbots():
     result = ''
 
     def __init__(self, problem_statement, mode):
+        logging.basicConfig(filename='stc.log', encoding='utf-8', level=logging.DEBUG)
+
         self.problem_statement = problem_statement
         self.mode = mode
 
         match self.mode:
+            case "idea":
+                self.renderIdea()
+                
             case "alternatives":
                 self.renderAlternatives()
 
-            case "somethingElse":
-                print("something else")
+            case "solutions":
+                self.renderSolutions()
+
+            case "feedback":
+                self.renderFeedback()
+
+            case "planning":
+                self.renderPlanning()
+
+            case "processimprovement":
+                self.renderProcessImprovement()
+
+            case "solvingproblems":
+                self.renderSolvingProblems()
+
+            case "review":
+                self.renderReview()
 
             case _:
                 print("Specify mode.")
 
 
+    def renderFeedback(self):   
+        return true
+    
+    def renderPlanning(self):   
+        return true
+
+    def renderProcessImprovement(self):   
+        return true
+    
+    def renderSolvingProblems(self):   
+        return true
+
+    def renderReview(self):   
+        return true
+
+    def renderSolutions(self):        
+        self.white_prompt = self.problem_statement + ' Describe your point of view to this problem, using only three bulletpoints. '
+        self.white_response = self.callChatGPT(self.white_system_content, self.white_prompt)
+
+        self.black_prompt = self.problem_statement + ' Describe your point of view to this problem, using only three bulletpoints.'
+        self.black_response = self.callChatGPT(self.black_system_content, self.black_prompt)
+
+        self.green_prompt = self.problem_statement + ' Describe your point of view to this problem, using only three bulletpoints. '
+        self.green_response = self.callChatGPT(self.green_system_content, self.green_prompt)
+
+        self.blue_prompt = 'Summarize the contributions of the participants, as listed beneath. Think about the problem statement and decide, based on the contributions of the participants, which alternative shoud be chosen. '
+        self.blue_prompt += 'The problem statement of the workshop is as follows: ' + self.problem_statement + '\n\n'
+        self.blue_prompt += 'Contribution from a fact-oriented participant: ' + self.white_response + '\n\n'
+        self.blue_prompt += 'Contribution from a emotion-oriented participant: ' + self.red_response + '\n\n'
+        self.blue_prompt += 'Contribution from a pessimist: ' + self.black_response + '\n\n'
+        self.blue_prompt += 'Contribution from an optimist: ' + self.yellow_response + '\n\n'
+        self.blue_prompt += 'Contribution from a innovation-oriented participant: ' + self.green_response
+        self.blue_response = self.callChatGPT(self.blue_system_content, self.blue_prompt)
+
+        self.result = '## White Hat\n'
+        self.result += '### Prompt\n' + self.white_system_content + '\n' + self.white_prompt + '\n'
+        self.result += '### Response\n' + self.white_response + '\n\n\n'
+
+        self.result += '## Black Hat\n'
+        self.result += '### Prompt\n' + self.black_system_content + '\n' + self.black_prompt + '\n'
+        self.result += '### Response\n' + self.black_response + '\n\n\n'
+
+        self.result += '## Green Hat\n'
+        self.result += '### Prompt\n' + self.green_system_content + '\n' + self.green_prompt + '\n'
+        self.result += '### Response\n' + self.green_response + '\n\n\n'
+
+        self.result += '## Blue Hat\n'
+        self.result += '### Prompt\n' + self.blue_system_content + '\n' + self.blue_prompt + '\n'
+        self.result += '### Response\n' + self.blue_response + '\n\n\n'
+
+
     def renderAlternatives(self):
         self.white_prompt = self.problem_statement + ' Describe your point of view to this problem, using only three bulletpoints. '
         self.white_response = self.callChatGPT(self.white_system_content, self.white_prompt)
+        logging.info('White prompt: %s \n White response: %s ', self.white_prompt, self.white_response)
 
         self.green_prompt = self.problem_statement + ' Describe your point of view to this problem, using only three bulletpoints. '
         self.green_response = self.callChatGPT(self.green_system_content, self.green_prompt)
@@ -103,10 +176,6 @@ class SixThinkingChatbots():
         self.blue_prompt += 'Contribution from a innovation-oriented participant: ' + self.green_response
         self.blue_response = self.callChatGPT(self.blue_system_content, self.blue_prompt)
 
-        self.renderAlternativesResults()
-
-
-    def renderAlternativesResults(self):
         self.result = '## White Hat\n'
         self.result += '### Prompt\n' + self.white_system_content + '\n' + self.white_prompt + '\n'
         self.result += '### Response\n' + self.white_response + '\n\n\n'
@@ -131,6 +200,33 @@ class SixThinkingChatbots():
         self.result += '### Prompt\n' + self.blue_system_content + '\n' + self.blue_prompt + '\n'
         self.result += '### Response\n' + self.blue_response + '\n\n\n'
   
+    def renderIdea(self):
+        self.white_prompt = self.problem_statement + ' Describe your point of view to this problem, using only three bulletpoints. '
+        self.white_response = self.callChatGPT(self.white_system_content, self.white_prompt)
+
+        self.green_prompt = self.problem_statement + ' Describe your point of view to this problem, using only three bulletpoints. '
+        self.green_response = self.callChatGPT(self.green_system_content, self.green_prompt)
+
+        self.blue_prompt = 'Summarize the contributions of the participants, as listed beneath. Think about the problem statement and decide, based on the contributions of the participants, which ideas shoud be chosen. '
+        self.blue_prompt += 'The problem statement of the workshop is as follows: ' + self.problem_statement + '\n\n'
+        self.blue_prompt += 'Contribution from a fact-oriented participant: ' + self.white_response + '\n\n'
+        self.blue_prompt += 'Contribution from a innovation-oriented participant: ' + self.green_response
+        self.blue_response = self.callChatGPT(self.blue_system_content, self.blue_prompt)
+        
+        self.result = '## White Hat\n'
+        self.result += '### Prompt\n' + self.white_system_content + '\n' + self.white_prompt + '\n'
+        self.result += '### Response\n' + self.white_response + '\n\n\n'
+
+        self.result += '## Green Hat\n'
+        self.result += '### Prompt\n' + self.green_system_content + '\n' + self.green_prompt + '\n'
+        self.result += '### Response\n' + self.green_response + '\n\n\n'
+
+        self.result += '## Blue Hat\n'
+        self.result += '### Prompt\n' + self.blue_system_content + '\n' + self.blue_prompt + '\n'
+        self.result += '### Response\n' + self.blue_response + '\n\n\n'
+
+
+
     def exportToMd(self):
         export = '# Six Thinking Chatbots ' + '\n'
         export += ' Mode: ' + self.mode + '\n'
@@ -154,13 +250,17 @@ class SixThinkingChatbots():
         time.sleep(0.1)
         return dialogue.choices[0].message.content
 
-problem = 'The goal of the workshop is to evaluate three alternatives to a problem. The problem is: '
-problem += 'The sales process on our companies website does not work very well. Management wants us to change the sales process to one of the three alternatives: '
-problem += 'First, an old-school chatbot with a beforehand written structure. '
-problem += 'Second, a chatbot using a large language model, without a pre-defined structure. '
-problem += 'Third, an old-school contact form. '
+#problemAlternativesMode = 'The goal of the workshop is to evaluate three alternatives to a problem. The problem is: '
+#problemAlternativesMode += 'The sales process on our companies website does not work very well. Management wants us to change the sales process to one of the three alternatives: '
+#problemAlternativesMode += 'First, an old-school chatbot with a beforehand written structure. '
+#problemAlternativesMode += 'Second, a chatbot using a large language model, without a pre-defined structure. '
+#problemAlternativesMode += 'Third, an old-school contact form. '
+#idea = SixThinkingChatbots(problemAlternativesMode, "alternatives")
 
-idea = SixThinkingChatbots(problem, "alternatives")
+problemIdeaMode = 'The goal of the workshop is to find new ideas. The problem is: '
+problemIdeaMode += 'The sales process on our companies website does not work very well. Management wants us to generate ideas how to improve the sales process.'
+idea = SixThinkingChatbots(problemIdeaMode, "idea")
+
 print(idea.exportToMd())
 
 
